3b525c04a9c5325d0e6c29c5dd39ed1e77226e2f
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-07 15:03:26 +0800

    L 1.4 & Challenge 1
    added vehicle controls to PlayerController
    completed Plane challenge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private int _speed = 20;
+    [SerializeField]
+    private int _turnSpeed = 50;
+
+    private float _horizontalInput;
+    private float _forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*Time.deltaTime*_speed);
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed * _forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * _turnSpeed * _horizontalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * _turnSpeed * _horizontalInput);
     }
 }
