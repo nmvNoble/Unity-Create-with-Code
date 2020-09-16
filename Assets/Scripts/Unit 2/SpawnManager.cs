a015cc505419b569a7c65d58b4fa7f12c5275b31
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-16 15:17:38 +0800

    L 2.3
    Random Animal Stampede
    
    Added SpawnManager Script that spawns a random animal prefab when S key pressed
    Changed camera to Orthographc view

---
+++ Assets/Scripts/Unit 2/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _animalPrefabs;
+    [SerializeField]
+    private int _spawnRangeX = 20, _spawnRangeZ = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-_spawnRangeX, _spawnRangeX), 0, _spawnRangeZ);
+            int animalIndex = Random.Range(0, _animalPrefabs.Length);
+            Instantiate(_animalPrefabs[animalIndex], spawnPos,
+                    _animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
