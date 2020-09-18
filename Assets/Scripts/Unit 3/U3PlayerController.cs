e1889dd158dd75a53b1a427ad4a45410d2742861
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-18 14:16:22 +0800

    L 3.1
    Set up Player and Obstacles
    Made Player jump and the Background and Obstacles now move left
    Made SpawnManager Script to invoke repeating Obstacles

---
+++ Assets/Scripts/Unit 3/U3PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U3PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumpForce = 10, _gravityMod = 1;
+
+    private Rigidbody _playerRB;
+    private bool _isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= _gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)//&& transform.position.y <= 0)
+        {
+            _playerRB.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _isOnGround = true;
+    }
+}
---
+++ Assets/Scripts/Unit 3/U3SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U3SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float _startDelay = 2, _repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", _startDelay, _repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(_obstaclePrefab, spawnPos, _obstaclePrefab.transform.rotation);
+    }
+}
