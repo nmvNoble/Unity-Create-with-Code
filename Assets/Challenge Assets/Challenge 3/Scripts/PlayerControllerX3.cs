e271d42018aa1e36be4680b0e8e436fe89e5ecbc
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-20 13:41:43 +0800

    Challenge 3
    Balloons, Bombs, & Booleans + Bonus

---
+++ Assets/Challenge Assets/Challenge 3/Scripts/PlayerControllerX3.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX3 : MonoBehaviour
+{
+    public bool gameOver = false;
+
+    public float floatForce = 1;
+    private float gravityModifier = 1.5f, _lowerBound = 1.5f;
+    private bool _isLowEnough = true;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip boingSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        playerRb = GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 2, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 14)
+            _isLowEnough = false;
+        else
+            _isLowEnough = true;
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver && _isLowEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+
+        if (!gameOver && transform.position.y <= _lowerBound)
+        {
+            playerRb.AddForce(Vector3.up * 3, ForceMode.Impulse);
+            playerAudio.PlayOneShot(boingSound, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+    }
+
+}
---
+++ Assets/Challenge Assets/Challenge 3/Scripts/RepeatBackgroundX3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX3 : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+    //private PlayerControllerX3 _playerCtrl;
+    private bool _gameOver;
+
+    private void Start()
+    {
+        startPos = transform.position; // Establish the default starting position 
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Set repeat width to half of the background
+        _gameOver = GameObject.Find("Player").GetComponent<PlayerControllerX3>().gameOver;
+    }
+
+    private void Update()
+    {
+        // If background moves left by its repeat width, move it back to start position
+        if (!_gameOver && transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+
+ 
+}
+
+
