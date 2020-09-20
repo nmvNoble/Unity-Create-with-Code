d1092ab1fcac1c026c8a413b4db55becd6d099c3
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-20 12:26:59 +0800

    L 3.4
    Particles and Sound Effects
    
    Added dirt and explosion particles to player
    Added main BGM to camera and added jump and crash SFX to player
    Edited PlayerController script to properly implement particles and SFX

--- Assets/Scripts/Unit 3/U3PlayerController.cs
+++ Assets/Scripts/Unit 3/U3PlayerController.cs
@@ -6,12 +6,16 @@ public class U3PlayerController : MonoBehaviour
 {
     public bool gameOver = false;
 
-    private Animator _playerAnim;
-
+    [SerializeField]
+    private AudioClip _jumpSFX, _crashSFX;
+    [SerializeField]
+    private ParticleSystem _explosionParticle, _dirtParticle;
     [SerializeField]
     private float _jumpForce = 10, _gravityMod = 1;// 17, 5 in mine
 
     private Rigidbody _playerRB;
+    private Animator _playerAnim;
+    private AudioSource _playerAudio;
     private bool _isOnGround = true;
 
     // Start is called before the first frame update
@@ -19,6 +23,7 @@ public class U3PlayerController : MonoBehaviour
     {
         _playerRB = GetComponent<Rigidbody>();
         _playerAnim = GetComponent<Animator>();
+        _playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= _gravityMod;
     }
 
@@ -30,19 +35,28 @@ public class U3PlayerController : MonoBehaviour
             _playerRB.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _isOnGround = false;
             _playerAnim.SetTrigger("Jump_trig");
+            _dirtParticle.Stop();
+            _playerAudio.PlayOneShot(_jumpSFX, 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
+        {
             _isOnGround = true;
+            _dirtParticle.Play();
+        }
+            
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Oof");
             _playerAnim.SetBool("Death_b", true);
             _playerAnim.SetInteger("DeathType_int", 1);
+            _explosionParticle.Play();
+            _dirtParticle.Stop();
+            _playerAudio.PlayOneShot(_crashSFX, 1.0f);
         }
     }
 }
