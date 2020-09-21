82b63846fbf8cbabc7ef84ed3fa81ca04cb371e1
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-21 16:45:47 +0800

    Initial set up & L 4.1
    Watch Where You’re Going
    
    Added Sphere Player
    Creted script to move player forward and turn camera when player turns

---
+++ Assets/Scripts/Unit 4/U4PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U4PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody _playerRB;
+    private GameObject focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        _playerRB.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        if (transform.position.y <= -1)
+            transform.position = Vector3.zero;
+    }
+}
