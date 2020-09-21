02d885c8e2e18ad534a4e6d135f3007c46f1da46
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-09-21 17:39:08 +0800

    L 4.2
    Follow the Player
    
    Added Physics Material Bouncy to player and enemy
    Enemy now follows Player
    Randomply generate spawn point of Enemy

---
+++ Assets/Scripts/Unit 4/U4Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U4Enemy : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody enemyRB;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAt = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce( lookAt * speed);
+    }
+}
---
+++ Assets/Scripts/Unit 4/U4SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U4SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    private float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        Instantiate(enemyPrefab, randPos, enemyPrefab.transform.rotation);
+    }
+}
