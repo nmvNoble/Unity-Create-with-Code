a69e1d1eedb6bf364d0ef066facf3d79fdd83a96
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-10-05 18:04:30 +0800

    L 5.1
    Clicky Mouse
    
    Every 1 second spawns 3 good targets and 1 bad targets in a random position with random force and torque
    Targets get destroyed on click or when hitting the sensor
    adjested default spawn settings

---
+++ Assets/Scripts/Unit 5/Target.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    private float _minSpd = 10, _maxSpd = 14, _maxTrq = 10, _xRange = 4, _ySpawnPos = -1;
+    private Rigidbody _targetRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetRB = GetComponent<Rigidbody>();
+        _targetRB.AddForce(RandForce(), ForceMode.Impulse);
+        _targetRB.AddTorque(RandTorque(), RandTorque(), RandTorque(), ForceMode.Impulse);
+        transform.position = RandSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    Vector3 RandSpawnPos()
+    {
+        return new Vector3(Random.Range(-_xRange, _xRange), _ySpawnPos);
+    }
+
+    Vector3 RandForce()
+    {
+        return Vector3.up * Random.Range(_minSpd, _maxSpd);
+    }
+
+    float RandTorque()
+    {
+        return Random.Range(-_maxTrq, _maxTrq);
+    }
+}
---
+++ Assets/Scripts/Unit 5/U5GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class U5GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnRate = 1.0f;
+    public List<GameObject> targets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+}
