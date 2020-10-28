72905bc0948857f8393542b55863472cbba9ae94
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-10-28 13:26:07 +0800

    Challenge 1 Paths & Reset
    Organized positioning of Obstacle Paths
    Counts target paths cleared
    Resets Game when colliding with obstacle
    Resets plane position when too far away from obstacle area

--- Assets/Challenge Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
     public float verticalInput;
+    public int cleared;
+
+    [SerializeField] private TextMeshProUGUI _pathsClearedText;
+    private Vector3 _respawnPos;
+    private Quaternion _respawnRot;
+    private GameManagerX1 _gm;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        _respawnPos = gameObject.transform.position;
+        _respawnRot = gameObject.transform.rotation;
+        _gm = GameObject.Find("Game Manager").GetComponent<GameManagerX1>();
     }
 
     // Update is called once per frame
@@ -25,5 +34,33 @@ public class PlayerControllerX : MonoBehaviour
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+
+        if(gameObject.transform.position.z > 250 ||
+                    gameObject.transform.position.z < -25 ||
+                    gameObject.transform.position.y > 100 ||
+                    gameObject.transform.position.y < -100)
+        {
+            ResetPos();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Path")
+        {
+            cleared++;
+            other.gameObject.SetActive(false);
+            _pathsClearedText.SetText("Target Path Cleared: " + cleared);
+        }
+        else
+        {
+            ResetPos();
+            _gm.ResetGame();
+        }
+    }
+
+    public void ResetPos()
+    {
+        gameObject.transform.position = _respawnPos;
+        gameObject.transform.rotation = _respawnRot;
     }
 }
