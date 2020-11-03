ed77a85876f553797047055c05752b1ff7810d3b
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-11-03 13:51:37 +0800

    C1 Levels
    Added levels to game, every total clear adds an obstacle path up to 5 of them
    Added Total Clear count text, Total Clear is every time the plane makes it back to the start

--- Assets/Challenge Assets/Challenge 1/Scripts/Extra/GameManagerX1.cs
+++ Assets/Challenge Assets/Challenge 1/Scripts/Extra/GameManagerX1.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManagerX1 : MonoBehaviour
 {
-    public int cleared;
+    public int cleared, level, totalClears;
     public bool isCompletePath = false, isReversePath = false;
     [SerializeField] private List<GameObject> _obstacles;
     [SerializeField] private GameObject _instStart, _instReverse, _instRestart;
+    [SerializeField] private TextMeshProUGUI _totalClearsText;
 
     // Start is called before the first frame update
     void Start()
     {
+        level = 1;
+        totalClears = 0;
         //Debug.Log("Count" + _obstacles.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ( cleared != 0 && (cleared % _obstacles.Count) == 0 && !isCompletePath)
+        if ( cleared != 0 && (cleared % level) == 0 && !isCompletePath)//(cleared % _obstacles.Count)
         {
             Debug.Log("cleared: ("+cleared+") % _obstacles.Count: ("+ _obstacles.Count + ") = " + 
                     cleared % _obstacles.Count);
@@ -41,6 +45,15 @@ public class GameManagerX1 : MonoBehaviour
                 _instRestart.SetActive(true);
                 _instReverse.SetActive(false);
                 Debug.Log("goin forward, isReversePath: " + isReversePath);
+                totalClears++;
+                _totalClearsText.SetText("Total Clears: " + totalClears);
+                if (level < 5)
+                    level++;
+                for (int i = 0; i < level; i++)//foreach (GameObject o in Obstacles)
+                {
+                    Debug.Log("i: " + i + ". Obstacle[i]: " + _obstacles[i]);
+                    _obstacles[i].SetActive(true);
+                }
             }
             StartCoroutine(TimedPathReset(_obstacles));
         }
