abc0b854d6bbb04ecf196cf718077674c708dfa5
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-11-04 13:19:53 +0800

    U1 Game over
    Added timer, 60 sec till game over
    Added Game over text
    Freeze time and moves Reset Game Button to center on game over

--- Assets/Scripts/Unit 1/U1 Extra/U1GameManager.cs
+++ Assets/Scripts/Unit 1/U1 Extra/U1GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
@@ -11,6 +12,12 @@ public class U1GameManager : MonoBehaviour
 
     [SerializeField] private U1PlayerController _player;
     [SerializeField] private TextMeshProUGUI _obstacleCountText, _clearsCountText;
+
+    public bool isGameActive;
+    public int timer = 60;
+    public Button restartButton;
+    [SerializeField] private Text timeText, gameOverText;
+    private float _timeRemaining;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +25,8 @@ public class U1GameManager : MonoBehaviour
         levelObstacles = 1;
         clearedLevels = 0;
         _obstacleCountText.SetText("Obstacles: " + activeObstacles);
+        isGameActive = true;
+        StartCoroutine(StartCountdown(timer));
     }
 
     // Update is called once per frame
@@ -33,13 +42,38 @@ public class U1GameManager : MonoBehaviour
             _obstacleCountText.SetText("Obstacles: " + levelObstacles);
             for (int i = 0; i < levelObstacles; i++)//foreach (GameObject o in Obstacles)
             {
-                Debug.Log("i: " + i + ". Obstacle[i]: " + Obstacles[i]);
+                //Debug.Log("i: " + i + ". Obstacle[i]: " + Obstacles[i]);
                 Obstacles[i].SetActive(true);
             }
             activeObstacles = levelObstacles;
         }
         if (Input.GetKeyDown(KeyCode.Escape))
             ResetGame();
+    }
+
+    public IEnumerator StartCountdown(float countdownValue)
+    {
+        _timeRemaining = countdownValue;
+        while (_timeRemaining > 0 && isGameActive)
+        {
+            yield return new WaitForSeconds(1.0f);
+            _timeRemaining--;
+            timeText.text = "Time: " + _timeRemaining;
+            if (_timeRemaining == 0)
+                GameOver();
+        }
+    }
+
+    // Stop game, bring up game over text and restart button
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverText.gameObject.SetActive(true); 
+        restartButton.transform.GetComponent<RectTransform>().anchorMin = new Vector2(0.5f, 0.5f);
+        restartButton.transform.GetComponent<RectTransform>().anchorMax = new Vector2(0.5f, 0.5f);
+        RectTransform restartRT = restartButton.GetComponent<RectTransform>();
+        restartRT.anchoredPosition = new Vector3(0, -40, 0);
+        isGameActive = false;
     }
 
     public void FallenObstacle()
@@ -50,6 +84,8 @@ public class U1GameManager : MonoBehaviour
 
     public void ResetGame()
     {
+
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
