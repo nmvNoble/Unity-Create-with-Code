1725a646bf8f611fcc0286ac7cfa2dba002a4d02
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-11-11 15:00:16 +0800

    C2 Score
    Added Score text, add to score when ball caught
    Using same Game Manager as U2

---
+++ Assets/Challenge Assets/Challenge 2/Scripts/C2 Extras/GameManagerX2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagerX2 : MonoBehaviour
+{
+    public bool isGameOver;
+    public int score, wave;
+    [SerializeField] private Text _scoreText, _waveText;
+    [SerializeField] private GameObject _gameOverText, _resetButton;
+    private bool _isNewWave;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        score = 0;
+        wave = 1;
+        _isNewWave = true;
+        isGameOver = false;
+    }
+
+    private void Update()
+    {
+        if (_isNewWave == false && score % 20 == 0)
+        {
+            _isNewWave = true;
+            wave++;
+            _waveText.text = "Wave: " + wave;
+        }
+        if (isGameOver)
+            GameOver();
+    }
+
+    public void UpdateScore(int val)
+    {
+        _isNewWave = false;
+        score += val;
+        _scoreText.text = "Score: " + score;
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0;
+        _gameOverText.SetActive(true);
+        _resetButton.SetActive(true);
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Challenge Assets/Challenge 2/Scripts/DetectCollisionsX2.cs
+++ Assets/Challenge Assets/Challenge 2/Scripts/DetectCollisionsX2.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class DetectCollisionsX2 : MonoBehaviour
 {
+    private GameManagerX2 _gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = GameObject.Find("Game Manager").GetComponent<GameManagerX2>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log("Ball caught at x:" + Mathf.Floor(transform.position.x));
+        _gm.UpdateScore(1);
         Destroy(gameObject);
     }
 }
