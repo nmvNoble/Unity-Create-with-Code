e77844421524961b2219ed2d451ccf5d3260eb1d
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-11-29 12:15:57 +0800

    C4 Game Manager initial and Instructions
    Added slightly edited template of Game Manager
    Added Text Instructions

---
+++ Assets/Challenge Assets/Challenge 4/Scripts/C4 Extras/GameManagerX4.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagerX4 : MonoBehaviour
+{
+    public bool isGameOver;
+    public int score, level;
+    [SerializeField] private Text _scoreText, _levelText, _timeText;
+    [SerializeField] private GameObject _gameOverText, _resetButton;
+    private bool _isNewLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        score = 0;
+        level = 1;
+        _isNewLevel = true;
+        isGameOver = false;
+    }
+
+    private void Update()
+    {
+        /*if (_isNewLevel == false && score % 20 == 0)
+        {
+            _isNewLevel = true;
+            level++;
+            _levelText.text = "Level: " + level;
+        }*/
+        if (isGameOver)
+            GameOver();
+    }
+
+    public void UpdateScore(int val)
+    {
+        _isNewLevel = false;
+        score += val;
+        _scoreText.text = "Score: " + score;
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0;
+        _gameOverText.SetActive(true);
+        _resetButton.SetActive(true);
+    }
+
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
