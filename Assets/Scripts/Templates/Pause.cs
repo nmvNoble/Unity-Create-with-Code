fbd3ee741906dbbbce38a8dcfea34499d91f6d44
nmvNoble <34275538+nmvNoble@users.noreply.github.com>
2020-12-10 17:58:41 +0800

    Units & Challenges Pause
    Allows each Game to be paused and resumed, stopping time and allowing player to read the Introduction and controls
    Allows players to go back to the Main Menu via button

---
+++ Assets/Scripts/Templates/Pause.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public bool isPaused;
+    [SerializeField] private GameObject _pauseBtn, _resumeBtn, _pauseScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PauseOnClick()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        _pauseBtn.SetActive(false);
+        _resumeBtn.SetActive(true);
+        _pauseScreen.SetActive(true);
+    }
+
+    public void ResumeOnClick()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        _pauseBtn.SetActive(true);
+        _resumeBtn.SetActive(false);
+        _pauseScreen.SetActive(false);
+    }
+
+    public void LoadCWCMenu()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
